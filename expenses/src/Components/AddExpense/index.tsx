b5ba219c332107e7bb82fedb6fd57ef027fd33f5
edpadua <ed.padua@gmail.com>
2023-07-06 02:07:00 -0300

    Media queries
    Media queries

--- expenses/src/Components/AddExpense/index.tsx
+++ expenses/src/Components/AddExpense/index.tsx
@@ -45,21 +45,21 @@ function AddExpense() {
       <div className="text-3xl font-bold pb-4">
           <h2>Despesa</h2>
       </div>
-      <form onSubmit={onSubmit} className='flex items-center space-x-6'>
+      <form onSubmit={onSubmit} className='lg:flex md:block  items-center lg:space-x-6'>
 
         <label className="font-bold" htmlFor="name">Nome</label>
         <input
           value={name} onChange={(e) => setName(e.target.value)}
-          className="w-1/4 placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="text" placeholder="Digite o nome da despesa ..."
+          className="lg:w-1/4 w-full placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md py-2 lg:space-x-0 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="text" placeholder="Digite o nome da despesa ..."
         />
     
         <label className="font-bold" htmlFor="amount">Valor</label>
         <input
           value={amount} onChange={(e) => setAmount(parseInt(e.target.value))}
-          className="w-1/4 placeholder:text-slate-400 block bg-white border border-slate-300 h-9 rounded-md py-2 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="number" placeholder="Digite o valor da despesa ..."
+          className="lg:w-1/4 w-full placeholder:text-slate-400 block bg-white border border-slate-300 h-9 rounded-md py-2 lg:space-x-0 pl-2 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" type="number" placeholder="Digite o valor da despesa ..."
         />
 
-        <button className="hover:bg-blue-800 bg-indigo-600 px-4 py-2 font-semibold h-9 text-white rounded-md shadow-sm sm:text-sm">Adicionar</button>
+        <button className="lg:mt-0 mt-2 hover:bg-blue-800 bg-indigo-600 px-4 py-2 font-semibold h-9 text-white rounded-md shadow-sm sm:text-sm">Adicionar</button>
       </form>
     </div>
   )
